38582de4e64a8a520d521752310e2a780cc5f937
Raju Sharma <raju.chapagain@outlook.com>
2023-03-13 19:44:09 +1100

    blob sample

---
+++ BlobAppSample/Program.cs
@@ -0,0 +1,11 @@
+﻿using Azure.Storage.Blobs;
+
+string connectionString = "connection-string-goes here";
+string containerName = "container-name-goes-here";
+string blobName = "script.sql";
+string filePath = @"...\script.sql";
+
+BlobClient blobClient = new BlobClient(connectionString, containerName, blobName);
+await blobClient.DownloadToAsync(filePath);
+
+Console.WriteLine("The blob container downloaded");
